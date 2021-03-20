d532b8909e695ab56d65b23fc0e330ff55842441
sonerrtgn <56644165+sonerrtgn@users.noreply.github.com>
2021-03-20 03:52:54 +0300

    Update baglanti.php

--- A_Anlık online mesajlasma php ajax javascript/baglanti.php
+++ A_Anlık online mesajlasma php ajax javascript/baglanti.php
@@ -3,7 +3,7 @@
 /**
  * Sistemi denemek veya kurmak için bir database açıp ismini $dbname değişkenine yazınız.
  * Açmış olduğunuz database'ye kullaniciler adinda bir tablo oluşturunuz:
- * Açtığınız kullanicilar tablosunu 6 kolonlu oluşturunuz 
+ * Açtığınız kullaniciler tablosunu 6 kolonlu oluşturunuz 
  * 1.kolon adi, 2.kolon soyadi, 3.kolon kullanici_adi, 4.kolon e_posta, 5.kolon sifre ve 6.kolon ise id olmalıdır, id'nin dışındakilerin hepsinin
  * türü text, id nin ise türü int ve AUTO_INCREMENT seçeneğinin açılmış  olması gerekmektedir.
  * Bu değişiklikleri yaptıktan sonra sisteminiz üzerinde deneyebilir, istediğiniz kısımları değiştirebilirsiniz.
